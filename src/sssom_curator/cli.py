b033f8dbb71695d15e22b30891da34dd48e49ff3
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-10-22 12:43:28 +0200

    Add

--- src/sssom_curator/cli.py
+++ src/sssom_curator/cli.py
@@ -1,5 +1,7 @@
 """Command line interface for :mod:`sssom_curator`."""
 
+import os
+import sys
 from pathlib import Path
 
 import click
@@ -10,19 +12,28 @@ __all__ = [
     "main",
 ]
 
+NAME = "sssom-curator.json"
 
-@click.group()
-@click.argument("path", type=Path)
+
+@click.group(help="A CLI for managing SSSOM repositories.")
+@click.option(
+    "-p",
+    "--path",
+    type=click.Path(file_okay=True, dir_okay=True, exists=True),
+    default=os.getcwd,
+    help=f"Either the path to a sssom-curator configuration file or a directory "
+    f"containing a file named {NAME}. Defaults to current working directory",
+)
 @click.pass_context
 def main(ctx: click.Context, path: Path) -> None:
-    """CLI for sssom_curator."""
+    """Run the CLI."""
     from .repository import Repository
 
-    if not path.exists():
-        raise FileNotFoundError
-
     if path.is_dir():
-        path = path.joinpath("sssom-curator.json")
+        path = path.joinpath(NAME)
+        if not path.is_file():
+            click.secho(f"no {NAME} found in directory {path}")
+            sys.exit(1)
 
     repository = Repository.model_validate_json(path.read_text())
     repository.update_relative_paths(directory=path.parent)
