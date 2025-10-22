a629474f2b2fd416ad3d3a64238b8113665722fe
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-10-22 23:05:47 +0200

    Update constants.py

--- src/sssom_curator/constants.py
+++ src/sssom_curator/constants.py
@@ -2,7 +2,6 @@
 
 from __future__ import annotations
 
-from collections.abc import Collection
 from functools import lru_cache
 from typing import Literal, TypeAlias
 
@@ -49,20 +48,3 @@ def _get_preferred() -> curies.Converter:
         uri_prefix_priority=["rdf", "default"],
         prefix_priority=["preferred", "default"],
     )
-
-
-def get_prefix_map(prefixes: Collection[str]) -> dict[str, str]:
-    """Get a CURIE map containing only the relevant prefixes."""
-    import bioregistry
-
-    prefix_map = {}
-    for prefix in sorted(prefixes, key=str.casefold):
-        resource = bioregistry.get_resource(prefix)
-        if resource is None:
-            raise KeyError
-        uri_prefix = resource.get_rdf_uri_prefix() or resource.get_uri_prefix()
-        if uri_prefix is None:
-            raise ValueError(f"could not look up URI prefix for {prefix}")
-        preferred_prefix = resource.get_preferred_prefix() or prefix
-        prefix_map[preferred_prefix] = uri_prefix
-    return prefix_map
