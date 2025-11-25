cb5c32d7c0136a274fd5510eaa3b94f33dd691c5
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-11-25 09:16:21 +0100

    Fix initialization of negative mappings (#15)

--- tests/test_init.py
+++ tests/test_init.py
@@ -7,7 +7,7 @@ from textwrap import dedent
 
 from sssom_pydantic import MappingSet
 
-from sssom_curator.constants import POSITIVES_NAME, PREDICTIONS_NAME
+from sssom_curator.constants import NEGATIVES_NAME, POSITIVES_NAME, PREDICTIONS_NAME
 from sssom_curator.initialize import initialize_folder
 
 TEST_PURL_BASE = "https://example.com/test/"
@@ -141,6 +141,20 @@ class TestInitializeFolder(unittest.TestCase):
                 ex:1\t1\tskos:exactMatch\tex:2\t2\tsemapv:ManualMappingCuration\torcid:0000-0003-4423-4370
             """).rstrip(),
             positives_path.read_text().rstrip(),
+        )
+
+        negatives_path = self.directory.joinpath("data", NEGATIVES_NAME)
+        self.assertTrue(negatives_path.is_file())
+        self.assertEqual(
+            dedent(f"""\
+                #curie_map:
+                #  ex: https://example.org/
+                #  skos: http://www.w3.org/2004/02/skos/core#
+                #mapping_set_id: https://example.org/ms/components/{NEGATIVES_NAME}
+                subject_id\tsubject_label\tpredicate_id\tpredicate_modifier\tobject_id\tobject_label\tmapping_justification\tauthor_id
+                ex:3\t3\tskos:exactMatch\tNot\tex:4\t4\tsemapv:ManualMappingCuration\torcid:0000-0003-4423-4370
+            """).rstrip(),
+            negatives_path.read_text().rstrip(),
         )
 
         predictions_path = self.directory.joinpath("data", PREDICTIONS_NAME)
