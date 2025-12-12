4bb018f4bffb2b0ddd608e7d0b0aa12826adaffa
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-12-12 13:34:08 +0100

    Externalize curation function (#35)

--- tests/test_wsgi.py
+++ tests/test_wsgi.py
@@ -1,5 +1,6 @@
 """Test the web app."""
 
+from collections.abc import Sequence
 from pathlib import Path
 from typing import ClassVar
 
@@ -13,7 +14,9 @@ from curies.vocabulary import (
     manual_mapping_curation,
     narrow_match,
 )
+from pydantic import BaseModel
 from sssom_pydantic import MappingTool, SemanticMapping
+from sssom_pydantic.process import UNSURE
 
 from sssom_curator.constants import NEGATIVES_NAME, POSITIVES_NAME, UNSURE_NAME
 from sssom_curator.web.components import Controller, State
@@ -42,17 +45,25 @@ TEST_PREDICTED_MAPPING_MARKED_TRUE = SemanticMapping(
     justification=manual_mapping_curation.pair.to_pydantic(),
     authors=[TEST_USER],
 )
+TEST_PREDICTED_MAPPING_MARKED_UNSURE = SemanticMapping(
+    subject=NamedReference.from_curie("chebi:133530", name="tyramine sulfate"),
+    predicate=exact_match,
+    object=NamedReference.from_curie("mesh:C027957", name="tyramine O-sulfate"),
+    justification=lexical_matching_process.pair.to_pydantic(),
+    confidence=0.95,
+    mapping_tool=MappingTool(name="test", version=None),
+    curation_rule_text=[UNSURE],
+)
 TEST_PREDICTED_MAPPING_MARKED_BROAD = SemanticMapping(
     subject=NamedReference.from_curie("chebi:133530", name="tyramine sulfate"),
-    # note this is flipped because
-    predicate=narrow_match,
+    predicate=broad_match,
     object=NamedReference.from_curie("mesh:C027957", name="tyramine O-sulfate"),
     justification=manual_mapping_curation.pair.to_pydantic(),
     authors=[TEST_USER],
 )
 TEST_PREDICTED_MAPPING_MARKED_NARROW = SemanticMapping(
     subject=NamedReference.from_curie("chebi:133530", name="tyramine sulfate"),
-    predicate=broad_match,
+    predicate=narrow_match,
     object=NamedReference.from_curie("mesh:C027957", name="tyramine O-sulfate"),
     justification=manual_mapping_curation.pair.to_pydantic(),
     authors=[TEST_USER],
@@ -110,6 +121,15 @@ class TestFull(cases.RepositoryTestCase):
         self.assertTrue(path.is_file())
         mappings, _, _ = sssom_pydantic.read(path)
         self.assertEqual(n, len(mappings), msg=f"{path.name} had the wrong number of mappings")
+
+    def assert_models_equal(
+        self, expected: Sequence[BaseModel], actual: Sequence[BaseModel]
+    ) -> None:
+        """Assert that a list of models are equal."""
+        self.assertEqual(
+            [m.model_dump(exclude_none=True, exclude_unset=True) for m in expected],
+            [m.model_dump(exclude_none=True, exclude_unset=True) for m in actual],
+        )
 
     def test_query(self) -> None:
         """Test making a query."""
@@ -169,7 +189,9 @@ class TestFull(cases.RepositoryTestCase):
         )
         self.assertIsNone(mapping_set.title)
         self.assertEqual(f"{self.purl_base}{POSITIVES_NAME}", mapping_set.id)
-        self.assertEqual([TEST_POSITIVE_MAPPING, TEST_PREDICTED_MAPPING_MARKED_TRUE], mappings)
+        self.assert_models_equal(
+            [TEST_POSITIVE_MAPPING, TEST_PREDICTED_MAPPING_MARKED_TRUE], mappings
+        )
 
         self.assert_file_mapping_count(self.controller.repository.negatives_path, 0)
         self.assert_file_mapping_count(self.controller.repository.predictions_path, 0)
@@ -193,7 +215,7 @@ class TestFull(cases.RepositoryTestCase):
         )
         self.assertIsNone(mapping_set.title)
         self.assertEqual(f"{self.purl_base}{NEGATIVES_NAME}", mapping_set.id)
-        self.assertEqual([TEST_PREDICTED_MAPPING_MARKED_FALSE], mappings)
+        self.assert_models_equal([TEST_PREDICTED_MAPPING_MARKED_FALSE], mappings)
 
         self.assert_file_mapping_count(self.controller.repository.positives_path, 1)
         self.assert_file_mapping_count(self.controller.repository.predictions_path, 0)
@@ -217,7 +239,7 @@ class TestFull(cases.RepositoryTestCase):
         )
         self.assertIsNone(mapping_set.title)
         self.assertEqual(f"{self.purl_base}{UNSURE_NAME}", mapping_set.id)
-        self.assertEqual([TEST_PREDICTED_MAPPING_MARKED_TRUE], mappings)
+        self.assert_models_equal([TEST_PREDICTED_MAPPING_MARKED_UNSURE], mappings)
 
         self.assert_file_mapping_count(self.controller.repository.positives_path, 1)
         self.assert_file_mapping_count(self.controller.repository.predictions_path, 0)
@@ -241,7 +263,9 @@ class TestFull(cases.RepositoryTestCase):
         )
         self.assertIsNone(mapping_set.title)
         self.assertEqual(f"{self.purl_base}{POSITIVES_NAME}", mapping_set.id)
-        self.assertEqual([TEST_POSITIVE_MAPPING, TEST_PREDICTED_MAPPING_MARKED_BROAD], mappings)
+        self.assert_models_equal(
+            [TEST_POSITIVE_MAPPING, TEST_PREDICTED_MAPPING_MARKED_BROAD], mappings
+        )
 
         self.assert_file_mapping_count(self.controller.repository.negatives_path, 0)
         self.assert_file_mapping_count(self.controller.repository.predictions_path, 0)
@@ -265,7 +289,9 @@ class TestFull(cases.RepositoryTestCase):
         )
         self.assertIsNone(mapping_set.title)
         self.assertEqual(f"{self.purl_base}{POSITIVES_NAME}", mapping_set.id)
-        self.assertEqual([TEST_POSITIVE_MAPPING, TEST_PREDICTED_MAPPING_MARKED_NARROW], mappings)
+        self.assert_models_equal(
+            [TEST_POSITIVE_MAPPING, TEST_PREDICTED_MAPPING_MARKED_NARROW], mappings
+        )
 
         self.assert_file_mapping_count(self.controller.repository.negatives_path, 0)
         self.assert_file_mapping_count(self.controller.repository.predictions_path, 0)
